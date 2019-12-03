80428aff85d3f0c09f005c2b9929916c47ecb1ee
Hosni Ghribi <ghribi.hosni7@gmail.com>
2019-12-03 11:53:55 +0100

    Updates

---
+++ precache-manifest.28930451e5aaceebbc6166e0e0e38225.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc0063d84c4ef94357fb126d2518611",
+    "url": "/react-to-do-list/index.html"
+  },
+  {
+    "revision": "860c653ba0a00c3ace16",
+    "url": "/react-to-do-list/static/css/main.42683c03.chunk.css"
+  },
+  {
+    "revision": "31d85c7177515f3de988",
+    "url": "/react-to-do-list/static/js/2.d3f9a996.chunk.js"
+  },
+  {
+    "revision": "860c653ba0a00c3ace16",
+    "url": "/react-to-do-list/static/js/main.445bf569.chunk.js"
+  },
+  {
+    "revision": "215006b9a4925982e5ec",
+    "url": "/react-to-do-list/static/js/runtime-main.2a37134d.js"
+  }
+]);
